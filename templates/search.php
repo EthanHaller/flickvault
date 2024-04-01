6099370c913cb8dfeb142f69ad0b039b99b5c6c4
Alec McCue <alecmccue14@gmail.com>
2024-03-31 23:17:32 -0400

    added fontawesome link

--- templates/search.php
+++ templates/search.php
@@ -20,6 +20,9 @@
     <link rel="stylesheet" href="styles/search.css">
     <link rel="stylesheet" href="styles/theme.css">
 
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <!-- Inter Google Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
