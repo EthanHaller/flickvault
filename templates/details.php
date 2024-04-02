814dec84b729d082746ac0d46c307db0f472e9b2
EthanHaller <ttk4ey@virginia.edu>
2024-04-02 19:13:49 -0400

    separate navbar

--- templates/details.php
+++ templates/details.php
@@ -28,37 +28,9 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg" data-bs-theme="dark">
-        <div class="container-fluid">
-            <a class="navbar-brand nav-title" aria-current="page" href="?command=home">
-                <h1>FlickVault</h1>
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="?command=watchlist">Watch List</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="?command=history">Watch History</a>
-                    </li>
-                </ul>
-                <form id="nav-search-form" class="d-flex" role="search" action="" method="get">
-                    <input type="hidden" name="command" value="search">
-                    <input class="form-control me-2" type="search" name="query" placeholder="Search" aria-label="Search">
-                    <button class="btn nav-search" type="submit">Search</button>
-                </form>
-
-                <div class="nav-item active ms-5">
-                    <a href="?command=logout" class="btn btn-danger rounded-circle p-2" style="width: 40px; height: 40px;">
-                        <i class="fa fa-sign-out text-white" style="font-size: 1.3rem; margin-left: 0.1rem;"></i>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php
+        include('/opt/src/flickvault/templates/navbar.php');
+    ?>
 
     <?php if (isset($_SESSION['movieDetails'])) : ?>
         <div class="container-fluid" style="padding-left: 0rem;">
@@ -103,9 +75,11 @@
                             <p class="details-actor"><?= $actor['name'] ?></p>
                         <?php endforeach; ?>
                     </div>
-                    <!-- add to watchlist button -->
                     <form method="post" action="?command=addToWatchlist&movieId=<?= $_SESSION['movieDetails']['id']; ?>&movieTitle=<?= $_SESSION['movieDetails']['title']; ?>&movieLength=<?= $_SESSION['movieDetails']['runtime']; ?>&moviePoster=https://image.tmdb.org/t/p/original/<?= $_SESSION['movieDetails']['poster_path']; ?>">
                         <button type="submit">Add to Watchlist</button>
+                    </form>
+                    <form method="post" action="?command=addToHistory&movieId=<?= $_SESSION['movieDetails']['id']; ?>&movieTitle=<?= $_SESSION['movieDetails']['title']; ?>&movieLength=<?= $_SESSION['movieDetails']['runtime']; ?>&moviePoster=https://image.tmdb.org/t/p/original/<?= $_SESSION['movieDetails']['poster_path']; ?>">
+                        <button type="submit">Add to History</button>
                     </form>
                 </div>
             </div>
--- templates/home.php
+++ templates/home.php
@@ -43,39 +43,9 @@
 
 <body>
   <header>
-    <nav class="navbar navbar-expand-lg" data-bs-theme="dark">
-      <div class="container-fluid">
-        <a class="navbar-brand nav-title" aria-current="page" href="?command=home">
-          <h1>FlickVault</h1>
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" href="?command=watchlist">Watch List</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="?command=history">Watch History</a>
-            </li>
-          </ul>
-
-          <form id="nav-search-form" class="d-flex" role="search" action="" method="get">
-            <input type="hidden" name="command" value="search">
-            <input class="form-control me-2" type="search" name="query" placeholder="Search" aria-label="Search">
-            <button class="btn nav-search" type="submit">Search</button>
-          </form>
-
-          <div class="nav-item active ms-5">
-            <a href="?command=logout" class="btn btn-danger rounded-circle p-2" style="width: 40px; height: 40px;">
-              <i class="fa fa-sign-out text-white" style="font-size: 1.3rem; margin-left: 0.1rem;"></i>
-            </a>
-          </div>
-
-        </div>
-      </div>
-    </nav>
+    <?php
+    include('/opt/src/flickvault/templates/navbar.php');
+    ?>
   </header>
 
   <div class="full-height-container">
